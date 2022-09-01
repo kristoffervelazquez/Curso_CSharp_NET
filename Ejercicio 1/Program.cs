d37e5c9c27e95af705ac501da2ba4317552acbb6
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-08-31 22:04:40 -0700

    Ejercicio de practica

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PesosToDolar());
+            Console.ReadKey();
+        }
+
+        public static double PesosToDolar()
+        {
+            Console.WriteLine("Cual es el precio del dolar hoy?");
+            double precioDolar = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese la cantidad en pesos que desea convertir: ");
+            int pesos = int.Parse(Console.ReadLine());
+
+
+            return pesos / precioDolar;
+        }
+
+
+
+
+        
+    }
+}
