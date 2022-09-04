f2127b28db1e4cf9d4b4138848f2ad75de1d992f
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-09-03 21:49:40 -0700

    Ejercicio 4 en el 2

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -14,11 +14,11 @@ namespace Ejercicio2
             //Escribir un programa que pida una palabra, y la escriba a su inversa, por ejemplo si se introduce Paris, devolverá "Sirap".
 
 
-           Console.WriteLine("Inrtroduce una palabra: ");
+            Console.WriteLine("Inrtroduce una palabra: ");
 
             string palabra = Console.ReadLine();
 
-            if(palabra != null)
+            if (palabra != null)
             {
                 char[] array = palabra.ToCharArray();
                 Array.Reverse(array);
@@ -27,6 +27,21 @@ namespace Ejercicio2
                 Console.WriteLine(palabra);
             }
             Console.ReadLine();
+
+
+            //Crear un programa que muestre las letras de la Z(mayúscula) a la A(mayúscula, descendiendo)
+
+            int i;
+            Console.Write("ABECEDARIO DESCENDENTE");
+            Console.WriteLine();
+
+            //Se recorren los numero de conversion de char segun su numero
+
+            for (i = 90; i >= 65; i--)
+            {
+                Console.WriteLine("LETRA: " + Convert.ToString((char)i));
+            }
+            Console.Write("Pulse una Tecla:"); Console.ReadLine();
         }
     }
 }
